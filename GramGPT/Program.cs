819240e2c3c672a1e794b2732155ce56b3e09d2a
comde <comde@DENNYSLAPTOP>
2023-04-16 22:58:20 +0300

    Add project files.

---
+++ GramGPT/Program.cs
@@ -0,0 +1,32 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var botClient = new TelegramBotClient("<bot:token>");
+
+using var cts = new CancellationTokenSource();
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdate,
+    pollingErrorHandler: HandleError,
+    cancellationToken: cts.Token
+);
+
+Console.WriteLine("Bot started... Press Enter to stop");
+Console.ReadLine();
+
+cts.Cancel();
+
+async Task HandleUpdate(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Type == UpdateType.Message && update.Message.Type == MessageType.Text)
+    {
+        var message = update.Message;
+        await botClient.SendTextMessageAsync(message.Chat.Id, $"You said: {message.Text}");
+    }
+}
+
+async Task HandleError(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    Console.WriteLine($"Error occurred: {exception.Message}");
+}
